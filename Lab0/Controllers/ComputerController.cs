e92b1a9a48b3a85f34b869bc49c0c5391c946315
mateusz.skrzypczak <mateuszskrzypczak6@gmail.com>
2025-10-21 16:38:56 +0200

    21.10

---
+++ Lab0/Controllers/ComputerController.cs
@@ -0,0 +1,32 @@
+﻿using Lab0.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Lab0.Controllers;
+
+public class ComputerController : Controller
+
+{
+    private static Dictionary<int, Computer> _computers = new();
+    private static int i = 0;
+    public IActionResult Index()
+    {
+        return View ();
+    }
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create(Computer model)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View();
+        }
+        return RedirectToAction("Index");
+    }
+}
---
+++ Lab0/Models/Computer.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab0.Models;
+
+public class Computer
+{
+    [HiddenInput]
+    public int Id { get; set; }
+
+    [StringLength(maximumLength: 60, MinimumLength = 1)]
+    public string Name { get; set; }
+    [StringLength(maximumLength: 40, MinimumLength = 1)]
+    public string Cpu { get; set; }
+    [StringLength(maximumLength: 50, MinimumLength = 1)]
+    public string Ram { get; set; }
+    [StringLength(maximumLength: 50, MinimumLength = 1)]
+    public string Gpu { get; set; }
+    [StringLength(maximumLength: 50, MinimumLength = 1)]
+    public string Producer { get; set; }
+    
+    [DataType(DataType.DateTime)]
+    public DateOnly ProductionDate { get; set; } 
+}
